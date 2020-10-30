660d2e3fe69757825f456b4672b76e8f800daf00
Evan K <EvanK311@gmail.com>
2020-10-29 20:36:58 -0700

    fitness plan schema added

--- models/fitnessplan.js
+++ models/fitnessplan.js
@@ -1,1 +1,20 @@
 const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const fitnessplanSchema = new Schema({
+    name: {
+        type: String,
+
+    },
+    workout: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "workout"
+        }
+    ]
+})
+
+const fitnessplan = mongoose.model("fitnessplan", fitnessplanSchema);
+
+module.exports = fitnessplan;

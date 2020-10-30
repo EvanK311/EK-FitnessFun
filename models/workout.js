9f3793f4a4f511750815f29e6a225a9ada52c2a2
Evan K <EvanK311@gmail.com>
2020-10-29 20:27:20 -0700

    created workout schema

--- models/workout.js
+++ models/workout.js
@@ -1,1 +1,32 @@
 const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+    name: {
+        type: String,
+        trim: true,
+        required: "You must enter a name of the workout."
+    },
+    type: {
+        type: String,
+        trim: true,
+        required: "Please input the type of workout."
+    },
+    weight: {
+        type: Number,
+        required: "Please input a used weight."
+    },
+    sets: {
+        type: Number,
+        required: "Please input a number of sets."
+    },
+    reps: {
+        type: Number,
+        required: "Please input a number of reps."
+    },
+    duration: {
+        type: Number,
+        required: "Please input a duration length."
+    },
+})

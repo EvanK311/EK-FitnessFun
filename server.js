ebc4dd494d8f43856e785cb58734f668e9dccd3c
Evan K <EvanK311@gmail.com>
2020-10-30 21:03:45 -0700

    began connections to front end. static html showing

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", { useNewUrlParser: true });
 
+    // creates the fitnessplan DB to hold different exercises
 // db.fitnessplan.create({ name: "test name" })
 //     .then(dbfitnessplan => {
 //         console.log(dbfitnessplan);
@@ -25,14 +26,14 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", { u
 //         console.log(message);
 //     });
 
-    
-db.workout.create({ name: "names" },{ type: "type" },{ weight: 5 },{ sets: 5 },{ reps: 5 },{ duration: 5 },)
-    .then(dbworkout => {
-        console.log(dbworkout);
-    })
-    .catch(({ message }) => {
-        console.log(message);
-    });
+    // create workout DB with different categories for workouts
+// db.workout.create({ name: "names" },{ type: "type" },{ weight: 5 },{ sets: 5 },{ reps: 5 },{ duration: 5 },)
+//     .then(dbworkout => {
+//         console.log(dbworkout);
+//     })
+//     .catch(({ message }) => {
+//         console.log(message);
+//     });
 
 app.get("/fitnessplan", (req, res) => {
     db.fitnessplan.find({})
@@ -58,9 +59,9 @@ app.get("/workout", (req, res) => {
 
 app.post("/submit", ({ body }, res) => {
     db.workout.create(body)
-        .then(({ _id }) => db.workout.findOneAndUpdate({}, { $push: { notes: _id } }, { new: true }))
-        .then(dbworkout => {
-            res.json(dbworkout);
+        .then(({ _id }) => db.fitnessplan.findOneAndUpdate({}, { $push: { workout: _id } }, { new: true }))
+        .then(dbfitnessplan => {
+            res.json(dbfitnessplan);
         })
         .catch(err => {
             res.json(err);
